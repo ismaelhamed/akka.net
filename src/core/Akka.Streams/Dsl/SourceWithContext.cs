05c08de40f99a7e2e4161e7e797782de9fa5539f
Ismael Hamed <1279846+ismaelhamed@users.noreply.github.com>
2021-05-23 15:07:08 +0200

    [WIP] Reorder Source/FlowWithContext type parameters

--- src/core/Akka.Streams/Dsl/SourceWithContext.cs
+++ src/core/Akka.Streams/Dsl/SourceWithContext.cs
@@ -6,7 +6,7 @@
 //-----------------------------------------------------------------------
 
 using System;
-using Akka.Streams.Implementation;
+using Akka.Annotations;
 
 namespace Akka.Streams.Dsl
 {
@@ -16,18 +16,16 @@ namespace Akka.Streams.Dsl
     /// use [[FlowWithContextOps.via]] to manually provide the context propagation for otherwise unsupported
     /// operations.
     /// 
-    /// Can be created by calling <see cref="Source{TOut,TMat}.StartContextPropagation"/>
-    /// 
-    /// API MAY CHANGE
+    /// Can be created by calling <see cref="SourceOperations.AsSourceWithContext{TOut,TCtx,TMat}"/>
     /// </summary>
-    public sealed class SourceWithContext<TCtx, TOut, TMat> : GraphDelegate<SourceShape<(TOut, TCtx)>, TMat>
+    [ApiMayChange]
+    public sealed class SourceWithContext<TOut, TCtx, TMat> : GraphDelegate<SourceShape<(TOut, TCtx)>, TMat>
     {
         public SourceWithContext(Source<(TOut, TCtx), TMat> source)
-        : base(source)
+            : base(source)
         {
         }
-        
-        
+
         ///<summary>
         /// Transform this flow by the regular flow. The given flow must support manual context propagation by
         /// taking and producing tuples of (data, context).
@@ -35,10 +33,9 @@ namespace Akka.Streams.Dsl
         /// This can be used as an escape hatch for operations that are not (yet) provided with automatic
         /// context propagation here.
         ///</summary>
-        public SourceWithContext<TCtx2, TOut2, TMat> Via<TCtx2, TOut2, TMat2>(
-            IGraph<FlowShape<(TOut, TCtx), (TOut2, TCtx2)>, TMat2> viaFlow) =>
-            new SourceWithContext<TCtx2, TOut2, TMat>(Source.FromGraph(Inner).Via(viaFlow));
-        
+        public SourceWithContext<TOut2, TCtx2, TMat> Via<TOut2, TCtx2, TMat2>(IGraph<FlowShape<(TOut, TCtx), (TOut2, TCtx2)>, TMat2> viaFlow) =>
+            new SourceWithContext<TOut2, TCtx2, TMat>(Source.FromGraph(Inner).Via(viaFlow));
+
         ///<summary>
         /// Transform this flow by the regular flow. The given flow must support manual context propagation by
         /// taking and producing tuples of (data, context).
@@ -49,16 +46,25 @@ namespace Akka.Streams.Dsl
         /// The <paramref name="combine"/> function is used to compose the materialized values of this flow and that
         /// flow into the materialized value of the resulting Flow.
         ///</summary>
-        public SourceWithContext<TCtx2, TOut2, TMat3> ViaMaterialized<TCtx2, TOut2, TMat2, TMat3>(
+        public SourceWithContext<TOut2, TCtx2, TMat3> ViaMaterialized<TOut2, TCtx2, TMat2, TMat3>(
             IGraph<FlowShape<(TOut, TCtx), (TOut2, TCtx2)>, TMat2> viaFlow, Func<TMat, TMat2, TMat3> combine) =>
-            new SourceWithContext<TCtx2, TOut2, TMat3>(Source.FromGraph(Inner).ViaMaterialized(viaFlow, combine));
+            new SourceWithContext<TOut2, TCtx2, TMat3>(Source.FromGraph(Inner).ViaMaterialized(viaFlow, combine));
 
-        
+        /// <summary>
+        /// Connect this <see cref="SourceWithContext{TOut,TCtx,TMat}"/> to a Sink and run it. The returned value is the materialized value of the Sink.
+        /// Note that the ActorSystem can be used as the implicit materializer parameter to use the SystemMaterializer for running the stream.
+        /// </summary>
+        /// <typeparam name="TMat2"></typeparam>
+        /// <param name="sink"></param>
+        /// <param name="materializer"></param>
+        /// <returns></returns>
+        public TMat2 RunWith<TMat2>(IGraph<SinkShape<(TOut, TCtx)>, TMat2> sink, IMaterializer materializer)
+            => Source.FromGraph(Inner).RunWith(sink, materializer);
+
         ///<summary>
         ///Stops automatic context propagation from here and converts this to a regular
         ///stream of a pair of (data, context).
         ///</summary>
-        public Source<(TOut, TCtx), TMat> AsSource() => 
-            Inner is Source<(TOut, TCtx), TMat>  source ? source : Source.FromGraph(Inner);
+        public Source<(TOut, TCtx), TMat> AsSource() => Inner is Source<(TOut, TCtx), TMat> source ? source : Source.FromGraph(Inner);
     }
 }
